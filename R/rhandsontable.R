25b95ac5f8e967187313d650956e4e11c7ce4189
xmusphlkg <60965585+xmusphlkg@users.noreply.github.com>
2022-07-17 20:36:53 +0800

    Update rhandsontable.R

--- R/rhandsontable.R
+++ R/rhandsontable.R
@@ -137,6 +137,8 @@ rhandsontable <- function(data, colHeaders, rowHeaders, comments = NULL,
     height = height,
     debug = ifelse(is.null(debug) || is.na(debug) || !is.numeric(debug), 0, debug),
     search = search,
+    filters = T,
+    dropdownMenu = T,
     licenseKey = 'non-commercial-and-evaluation'
   )
 
